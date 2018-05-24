ec06b2b0a319f308719b61596459c628e240f19b
Chrisostomos Bakouras <chbakouras@gmail.com>
2018-05-24 15:50:22 +0300

    wip

--- apps/job_scheduler/management/commands/cron.sh
+++ apps/job_scheduler/management/commands/cron.sh
@@ -1,18 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-PATH=$1
-MANAGE_PATH=$PATH/manage.py
+PROJECT_PATH=$1
+cd $PROJECT_PATH
+
+MANAGE_PATH=$PROJECT_PATH/manage.py
 COMMAND="pipenv run python3.5 $MANAGE_PATH scheduler"
-cd $PATH
 
-(/usr/bin/sleep 5 && eval $COMMAND) &
-(/usr/bin/sleep 10 && eval $COMMAND) &
-(/usr/bin/sleep 15 && eval $COMMAND) &
-(/usr/bin/sleep 20 && eval $COMMAND) &
-(/usr/bin/sleep 25 && eval $COMMAND) &
-(/usr/bin/sleep 30 && eval $COMMAND) &
-(/usr/bin/sleep 35 && eval $COMMAND) &
-(/usr/bin/sleep 40 && eval $COMMAND) &
-(/usr/bin/sleep 45 && eval $COMMAND) &
-(/usr/bin/sleep 50 && eval $COMMAND) &
-(/usr/bin/sleep 55 && eval $COMMAND) &
+(sleep 5 && eval $COMMAND) &
+(sleep 10 && eval $COMMAND) &
+(sleep 15 && eval $COMMAND) &
+(sleep 20 && eval $COMMAND) &
+(sleep 25 && eval $COMMAND) &
+(sleep 30 && eval $COMMAND) &
+(sleep 35 && eval $COMMAND) &
+(sleep 40 && eval $COMMAND) &
+(sleep 45 && eval $COMMAND) &
+(sleep 50 && eval $COMMAND) &
+(sleep 55 && eval $COMMAND) &
