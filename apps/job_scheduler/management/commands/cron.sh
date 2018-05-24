eccf3a44289baf2e9089256af7d08362f8784cd1
Chrisostomos Bakouras <chbakouras@gmail.com>
2018-05-24 15:02:22 +0300

    wip

--- apps/job_scheduler/management/commands/cron.sh
+++ apps/job_scheduler/management/commands/cron.sh
@@ -5,14 +5,14 @@ pipenv shell
 MANAGE_PATH=$1
 COMMAND="python3.5 $MANAGE_PATH scheduler"
 
-(sleep 5 && $COMMAND) &
-(sleep 10 && $COMMAND) &
-(sleep 15 && $COMMAND) &
-(sleep 20 && $COMMAND) &
-(sleep 25 && $COMMAND) &
-(sleep 30 && $COMMAND) &
-(sleep 35 && $COMMAND) &
-(sleep 40 && $COMMAND) &
-(sleep 45 && $COMMAND) &
-(sleep 50 && $COMMAND) &
-(sleep 55 && $COMMAND) &
+(sleep 5 && eval $COMMAND) &
+(sleep 10 && eval $COMMAND) &
+(sleep 15 && eval $COMMAND) &
+(sleep 20 && eval $COMMAND) &
+(sleep 25 && eval $COMMAND) &
+(sleep 30 && eval $COMMAND) &
+(sleep 35 && eval $COMMAND) &
+(sleep 40 && eval $COMMAND) &
+(sleep 45 && eval $COMMAND) &
+(sleep 50 && eval $COMMAND) &
+(sleep 55 && eval $COMMAND) &
